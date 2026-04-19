a1bebf215c0e8c360c5f733d4173794cb6ad72f9
Victoria Knight <vgknight1589@gmail.com>
2026-04-19 13:51:28 -0400

    finalized final project code

--- FinalProject/Assets/Scripts/DatabaseManager.cs
+++ FinalProject/Assets/Scripts/DatabaseManager.cs
@@ -1,29 +1,3 @@
-/*
-using UnityEngine;
-
-public class DatabaseManager : MonoBehaviour
-{
-    public static DatabaseManager Instance { get; private set; }
-
-    void Awake()
-    {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
-
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-    }
-
-    public void SaveScore(string playerName, int score, float time)
-    {
-        Debug.Log("Saving score: " + playerName + " " + score + " " + time);
-    }
-}
-*/
-
 using UnityEngine;
 using System.Data;
 using Mono.Data.Sqlite;
